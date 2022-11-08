b6c2e66934df0626a15d45bdc3ae673b9927f0d9
Leandro Falcon <cronosteo1034@gmail.com>
2022-11-08 03:16:47 -0300

    underline animation

--- js/preloader.js
+++ js/preloader.js
@@ -1,1 +1,15 @@
 
+const preloader = document.querySelector('.preloader');
+
+const fadeOutEffect = setInterval(() => {
+    if (!preloader.style.opacity) {
+      preloader.style.opacity = 1;
+    }
+    if (preloader.style.opacity > 0) {
+      preloader.style.opacity -= 0.1;
+    } else {
+      clearInterval(fadeOutEffect);
+    }
+}, 300);
+
+window.addEventListener('load', fadeOutEffect);
